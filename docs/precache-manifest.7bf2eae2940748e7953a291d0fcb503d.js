59ef7d53cb2b6991efd198b9f7aeeed3f4f4d9cd
Gianmarco <cavallogianmarco@gmail.com>
2020-05-12 17:59:00 +0200

    fixed (I hope)

---
+++ docs/precache-manifest.7bf2eae2940748e7953a291d0fcb503d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781d0f2999ca854c11c039f23f7e3fa7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ed14c3429c08904d6fb",
+    "url": "./static/css/main.29d818e1.chunk.css"
+  },
+  {
+    "revision": "f50e98ce58b8010e5f50",
+    "url": "./static/js/2.8d9e8ccb.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "./static/js/2.8d9e8ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed14c3429c08904d6fb",
+    "url": "./static/js/main.c069fffd.chunk.js"
+  },
+  {
+    "revision": "775fa4c39d5493aedd9e",
+    "url": "./static/js/runtime-main.567307e2.js"
+  },
+  {
+    "revision": "6ede4f9db124ac321ba2b6d022a7d4f0",
+    "url": "./static/media/logo.6ede4f9d.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed796ef14f4628f2e3da521b0aa6f316.js"
+  "./precache-manifest.7bf2eae2940748e7953a291d0fcb503d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
