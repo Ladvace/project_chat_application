54e7edea1d0525917bfeb3d1dc65ea8281ced595
Gianmarco <cavallogianmarco@gmail.com>
2020-05-12 17:48:05 +0200

    docs compiled version of client

---
+++ docs/precache-manifest.ed796ef14f4628f2e3da521b0aa6f316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a182fc0ffe2c24f4b5637f0a6544bcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ed14c3429c08904d6fb",
+    "url": "/static/css/main.29d818e1.chunk.css"
+  },
+  {
+    "revision": "a9cf2671f1642d961a24",
+    "url": "/static/js/2.e857b4e4.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.e857b4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed14c3429c08904d6fb",
+    "url": "/static/js/main.c069fffd.chunk.js"
+  },
+  {
+    "revision": "134f2913d0ede7310ff5",
+    "url": "/static/js/runtime-main.26112f1e.js"
+  },
+  {
+    "revision": "6ede4f9db124ac321ba2b6d022a7d4f0",
+    "url": "/static/media/logo.6ede4f9d.png"
+  }
+]);
